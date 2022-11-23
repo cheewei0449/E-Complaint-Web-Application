87d9811bf324e5f0fe14ff1b473cbb37e1186582
cheewei <chngcheewei0449@e.newera.edu.my>
2022-11-23 22:31:22 +0800

    header navbar change

--- project/customer_read.php
+++ project/customer_read.php
@@ -18,62 +18,9 @@ include 'check.php';
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-
-                <a class="navbar-brand " href="#">
-                    <i class="fa-brands fa-shopify fa-xl text-light me-2 "></i>
-                    eshop
-                </a>
-
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarCollapse">
-                    <ul class="navbar-nav mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="product_create.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Product
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
-                                <li><a class="dropdown-item" href="product_read.php">Product List</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="create_customer.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Customer
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="create_customer.php">Create Customer</a></li>
-                                <li><a class="dropdown-item" href="customer_read.php">Customer riew</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="create_new_order.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Order
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="create_new_order.php">Create Order</a></li>
-                                <li><a class="dropdown-item" href="order_read.php">Order List</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="Contact.php">Contact Us</a>
-                        </li>
-                        <li>
-                            <a class="btn btn-danger" href="logout.php">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?php
+    include "header_navbar.php";
+    ?>
     <!-- container -->
     <div class="container">
         <div class="page-header">
--- project/order_read.php
+++ project/order_read.php
@@ -17,147 +17,93 @@ include 'check.php';
 
 </head>
 
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
+<?php
+include "header_navbar.php";
+?>
+<!-- container -->
+<div class="container">
+    <div class="page-header">
+        <h1>Order List</h1>
+    </div>
 
-                <a class="navbar-brand " href="#">
-                    <i class="fa-brands fa-shopify fa-xl text-light me-2 "></i>
-                    eshop
-                </a>
+    <!-- PHP code to read records will be here -->
+    <?php
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    if ($_GET) {
+        $message = $_GET['message'];
 
-                <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarCollapse">
-                    <ul class="navbar-nav mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="product_create.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Product
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
-                                <li><a class="dropdown-item" href="product_read.php">Product List</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="create_customer.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Customer
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="create_customer.php">Create Customer</a></li>
-                                <li><a class="dropdown-item" href="customer_read.php">Customer riew</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="create_new_order.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Order
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="create_new_order.php">Create Order</a></li>
-                                <li><a class="dropdown-item" href="order_read.php">Order List</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="Contact.php">Contact Us</a>
-                        </li>
-                        <li>
-                            <a class="btn btn-danger" href="logout.php">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <!-- container -->
-    <div class="container">
-        <div class="page-header">
-            <h1>Order List</h1>
-        </div>
-
-        <!-- PHP code to read records will be here -->
-        <?php
-
-        if ($_GET) {
-            $message = $_GET['message'];
-
-            if ($message == "update_success") {
-                echo "<div class='alert alert-success'>Record was updated.</div>";
-            } else {
-                echo "<div class='alert alert-danger align-item-center'>Unknown error happened</div>";
-            }
-        }
-
-
-        // include database connection
-        include 'config/database.php';
-
-        // delete message prompt will be here
-
-        // select all data
-        $query = "SELECT OrderID, CustomerID, order_date FROM order_summary ORDER BY OrderID DESC";
-        $stmt = $con->prepare($query);
-        $stmt->execute();
-
-        // this is how to get number of rows returned
-        $num = $stmt->rowCount();
-
-        // link to create record form
-        echo "<a href='create_new_order.php' class='btn btn-primary m-b-1em'>Create New Order</a>";
-
-        //check if more than 0 record found
-        if ($num > 0) {
-
-            // data from database will be here
-            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
-
-            //creating our table heading
-            echo "<tr>";
-            echo "<th>Order ID</th>";
-            echo "<th>Customer ID</th>";
-            echo "<th>Order Date</th>";
-            echo "</tr>";
-
-            // table body will be here
-            // retrieve our table contents
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                // extract row
-                // this will make $row['firstname'] to just $firstname only
-                extract($row);
-                // creating new table row per record
-                echo "<tr>";
-                echo "<td>{$OrderID}</td>";
-                echo "<td>{$CustomerID}</td>";
-                echo "<td>{$order_date}</td>";
-                echo "<td>";
-                // read one record
-                echo "<a href='order_read_one.php?id={$OrderID}' class='btn btn-info m-r-1em'>Read</a>";
-
-                // we will use this links on next part of this post
-                echo "<a href='order_update.php?id={$OrderID}' class='btn btn-primary m-r-1em'>Edit</a>";
-
-                // we will use this links on next part of this post
-                echo "<a href='#' onclick='delete_product({$OrderID});'  class='btn btn-danger'>Delete</a>";
-                echo "</td>";
-                echo "</tr>";
-            }
-
-
-            // end table
-            echo "</table>";
+        if ($message == "update_success") {
+            echo "<div class='alert alert-success'>Record was updated.</div>";
         } else {
-            echo "<div class='alert alert-danger'>No records found.</div>";
+            echo "<div class='alert alert-danger align-item-center'>Unknown error happened</div>";
         }
-        ?>
+    }
 
-    </div> <!-- end .container -->
 
-    <!-- confirm delete record will be here -->
+    // include database connection
+    include 'config/database.php';
+
+    // delete message prompt will be here
+
+    // select all data
+    $query = "SELECT OrderID, CustomerID, order_date FROM order_summary ORDER BY OrderID DESC";
+    $stmt = $con->prepare($query);
+    $stmt->execute();
+
+    // this is how to get number of rows returned
+    $num = $stmt->rowCount();
+
+    // link to create record form
+    echo "<a href='create_new_order.php' class='btn btn-primary m-b-1em'>Create New Order</a>";
+
+    //check if more than 0 record found
+    if ($num > 0) {
+
+        // data from database will be here
+        echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+        //creating our table heading
+        echo "<tr>";
+        echo "<th>Order ID</th>";
+        echo "<th>Customer ID</th>";
+        echo "<th>Order Date</th>";
+        echo "</tr>";
+
+        // table body will be here
+        // retrieve our table contents
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            // extract row
+            // this will make $row['firstname'] to just $firstname only
+            extract($row);
+            // creating new table row per record
+            echo "<tr>";
+            echo "<td>{$OrderID}</td>";
+            echo "<td>{$CustomerID}</td>";
+            echo "<td>{$order_date}</td>";
+            echo "<td>";
+            // read one record
+            echo "<a href='order_read_one.php?id={$OrderID}' class='btn btn-info m-r-1em'>Read</a>";
+
+            // we will use this links on next part of this post
+            echo "<a href='order_update.php?id={$OrderID}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+            // we will use this links on next part of this post
+            echo "<a href='#' onclick='delete_product({$OrderID});'  class='btn btn-danger'>Delete</a>";
+            echo "</td>";
+            echo "</tr>";
+        }
+
+
+        // end table
+        echo "</table>";
+    } else {
+        echo "<div class='alert alert-danger'>No records found.</div>";
+    }
+    ?>
+
+</div> <!-- end .container -->
+
+<!-- confirm delete record will be here -->
 </body>
 <footer class="container">
     <p class="float-end"><a class="text-decoration-none fw-bold" href="#">Back to top</a></p>
