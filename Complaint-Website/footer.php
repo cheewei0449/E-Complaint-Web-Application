9c0617e842ea362e197224efa7fceb7ae63d7f84
cheewei <chngcheewei0449@e.newera.edu.my>
2023-01-14 14:34:38 +0800

    complaint

---
+++ Complaint-Website/footer.php
@@ -0,0 +1,5 @@
+<footer class="container">
+    <p class="float-end"><a class="text-decoration-none fw-bold" href="#">Back to top</a></p>
+    <p class="text-muted fw-bold">&copy;Complaint Website </p>
+</footer>
+<?php
--- project/check.php
+++
@@ -1,4 +0,0 @@
-<?php session_start();
-if (!isset($_SESSION["username"]) && !isset($_SESSION["password"])) {
-  header("Location: index.php?error=session_expired");
-}
